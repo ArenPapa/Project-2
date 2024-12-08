89d60289dd876a7307aef0c7e0d8c55cb3cd3b9d
Manaka <manaka3leche@gmail.com>
2024-12-08 21:29:45 +0900

    Update graphing.py

--- graphing.py
+++ graphing.py
@@ -4,9 +4,10 @@ import numpy as np
 import requests
 import datetime as dt
 import matplotlib.dates as mdates
-from matplotlib.pyplot import subplot
+from matplotlib.lines import lineStyles
+from matplotlib.pyplot import subplot, plot_date
 import matplotlib
-
+from matplotlib.ticker import MaxNLocator
 from graph_lib import *
 
 matplotlib.use("MacOSX")
@@ -18,144 +19,157 @@ data = request.json()
 
 readings = data['readings'][0]
 
-
 # getting all the data from the server
 sensor = {
-    185: {"name": "temp_dht", "values": [], "timestamps": []}, #change sensor_id
-    186: {"name": "hum_dht", "values": [], "timestamps": []},  #change sensor_id
-    187: {"name": "temp_bmp", "values": [], "timestamps": []}, #change sensor_id
-    188: {"name": "hum_bmp", "values": [], "timestamps": []},  #change sensor_id
-    197: {"name": "pressure_bmp", "values": [], "timestamps": []}, #change sensor_id
+    10: {"name": "temp", "values": [], "timestamps": []},
+    455: {"name": "hum", "values": [], "timestamps": []},
+    456: {"name": "pressure", "values": [], "timestamps": []},
 }
 
-
+count = 0
 for r in readings:
     if r['sensor_id'] in sensor:
-        sensor[r["sensor_id"]]["values"].append(r["value"])
-        sensor[r['sensor_id']]["timestamps"].append(dt.datetime.fromisoformat(r["datetime"]))
+        # getting the value and store them into each sensor_ib
+        sensor[r["sensor_id"]]["values"].append(r['value'])
+        timestamp = dt.datetime.fromisoformat(r['datetime'])
+        sensor[r['sensor_id']]['timestamps'].append(timestamp)
 
-# Temperature DHT ------------------------------------------------------------------------------------------------------
-plt.subplot(3,2,1)
+sensor[10]['values'] = sensor[10]['values'][0:1440]
+sensor[10]['timestamps'] = sensor[10]['timestamps'][0:1440]
+sensor[455]['values'] = sensor[455]['values'][0:1440]
+sensor[455]['timestamps'] = sensor[455]['timestamps'][0:1440]
+sensor[456]['values'] = sensor[456]['values'][0:1440]
+sensor[456]['timestamps'] = sensor[456]['timestamps'][0:1440]
 
-temp = sensor[185]['values']
-y_temp = smooth(50, temp)
-x = list(range(len(y_temp)))
-x_time = [dt.datetime.fromisoformat(date_str) for date_str in sensor[185]['datetime']]
+print(len(sensor[455]['values']))
 
-plt.plot(x,y_temp,label= "Temperature DHT",color='red')
-plt.plot(x, non_linear(x,y_temp),label = "Non-linear Model", color = 'blue')
-plt.axhline(float(np.mean(y_temp)), label="Mean", color='green')
-plt.axhline(float(np.std(y_temp)),label = "Standard Deviation", color = 'gold')
-plt.axhline(np.min(y_temp), label = "Minimum", color = 'purple')
-plt.axhline(np.max(y_temp), label = "Maximum", color = "pink")
-plt.axhline(float(np.median(y_temp)), label = "Median", color = "orange")
+#temperature------------------------------------------------------------------------------------------------------------
 
+plt.subplot(3,1,1)
+
+temp = sensor[10]['values'] # 48hours of temperature
+y_temp = smooth(50, temp) # moving_average
+x_time = sensor[10]['timestamps'][0:len(y_temp)] # datetime
+x = list(np.arange(len(y_temp))) # list of number of index
+
+minTemp = np.min(y_temp)
+minX = y_temp[0]
+for i in range(len(sensor[10]['values'])):
+    if sensor[10]['values'][i] == minTemp:
+        minX = x[i]
+
+maxTemp = np.max(y_temp)
+maxX_bmp = x[0]
+for i in range(len(y_temp)):
+    if sensor[10]['values'][i] == maxTemp:
+        maxX_bmp = x_time[i]
+
+plt.plot(x,y_temp, label = "Temperature BMP", color = 'r')
+plt.plot(x, non_linear(x,y_temp), label = "Model", color = 'b')
+plt.axhline(float(np.mean(y_temp)), label="Mean", color='g')
+plt.axhline(float(np.std(y_temp)), label = "Standard Deviation", color = 'gold')
+plt.scatter(minX, minTemp, label = "Minimum", color = 'purple')
+plt.scatter(maxX_bmp, maxTemp, label = 'Maximum', color = 'pink')
+plt.axhline(float(np.median(y_temp)), label = "Median", color = 'orange')
+print("Temperature Graph Succeed")
+
+# x_pred_time, y_pred_value = prediction_model(x_time,y_temp)
+# plt.plot(x_pred_time, y_pred_value, label = "Prediction", color = "black")
+# print("Graph Succeed")
+
+plt.title("Temperature")
+plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=6))  # Major ticks every 1 hour
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
+
+plt.legend()
+plt.xlabel('Time')
+plt.ylabel("Temperature")
+plt.xticks(rotation = 45)
+
+#humidity---------------------------------------------------------------------------------------------------------------
+plt.subplot(3,1,2)
+
+hum = sensor[455]['values']
+y_hum = smooth(50, hum)
+x = list(np.arange(len(y_hum))) # list of number of index
+x_time = sensor[455]['timestamps'] #datetime format
+
+min_hum = np.min(y_hum)
+minX_hum = x_time[0]
+for i in range(len(sensor[455]['values'])):
+    if sensor[455]['values'][i] == min_hum:
+        minX_hum = x[i]
+
+maxHum = np.max(y_hum)
+maxX_bmp = x[0]
+for i in range(len(y_hum)):
+    if sensor[455]['values'][i] == maxHum:
+        maxX_bmp = x_time[i]
+
+
+plt.plot(x,y_hum,label= "Humidity",color='red')
+plt.plot(x, non_linear(x,y_hum),label = "Non-linear Model", color = 'blue')
+plt.axhline(float(np.mean(y_hum)), label="Mean", color='green')
+plt.axhline(float(np.std(y_hum)),label = "Standard Deviation", color = 'gold')
+plt.plot(np.max(y_hum), label = "Maximum", color = "pink")
+plt.axhline(float(np.median(y_hum)), label = "Median", color = "orange")
+print("Humidity Graph Succeed")
 #prediction
-x_pred_time, y_pred_value = prediction_model(x_time,y_temp)
+x_pred_time, y_pred_value = prediction_model(x_time,y_hum)
 plt.plot(x_pred_time, y_pred_value, label = "Prediction", color = "black")
-
-plt.title("Temperature DHT")
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
-plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=3))
-plt.gca().xaxis.set_minor_locator(mdates.HourLocator(interval=1))  # Minor ticks every 1 hour
-plt.legend()
-plt.xlabel('Time')
-plt.ylabel("Temperature")
-
-# Temperature BMP-------------------------------------------------------------------------------------------------------
-plt.subplot(3,2,2)
-
-tempBMP = sensor[187]['values']
-y_temp_bmp = smooth(50, tempBMP)
-plt.plot(x,y_temp_bmp, label = "Temperature BMP", color = 'red')
-plt.plot(x, non_linear(x,y_temp_bmp), label = "Model", color = 'blue')
-plt.axhline(float(np.mean(y_temp_bmp)), label="Mean", color='green')
-plt.axhline(float(np.std(y_temp_bmp)), label = "Standard Deviation", color = 'gold')
-plt.axhline(np.min(y_temp_bmp), label = "Minimum", color = 'purple')
-plt.axhline(np.max(y_temp_bmp), label = 'Maximum', color = 'pink')
-plt.axhline(float(np.median(y_temp_bmp)), label = "Median", color = 'orange')
-
-x_pred_time, y_pred_value = prediction_model(x_time,y_temp_bmp)
-plt.plot(x_pred_time, y_pred_value, label = "Prediction", color = "black")
-
-plt.title("Temperature BMP")
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
-plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=3))
-plt.gca().xaxis.set_minor_locator(mdates.HourLocator(interval=1))  # Minor ticks every 1 hour
-plt.legend()
-plt.xlabel('Time')
-plt.ylabel("Temperature")
-
-# Humidity DHT----------------------------------------------------------------------------------------------------------
-plt.subplot(3,2,3)
-
-humDHT = sensor[186]['values']
-y_hum_dht = smooth(50, humDHT)
-plt.plot(x, y_hum_dht, label = "Humidity DHT", color = 'red')
-plt.plot(x, non_linear(x,y_hum_dht), label = 'Model', color = 'blue')
-plt.axhline(float(np.mean(y_hum_dht)), label="Mean", color='green')
-plt.axhline(float(np.std(y_hum_dht)), label = "Standard Deviation", color = 'gold')
-plt.axhline(np.min(y_hum_dht), label = "Minimum", color = 'purple')
-plt.axhline(np.max(y_hum_dht), label = 'Maximum', color = 'pink')
-plt.axhline(float(np.median(y_hum_dht)), label = "Median", color = 'orange')
-
-x_pred_time, y_pred_value = prediction_model(x_time,y_hum_dht)
-plt.plot(x_pred_time, y_pred_value, label = "Prediction", color = "black")
+print("Prediction Succeed")
 
 plt.title("Humidity DHT")
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
-plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=3))
-plt.gca().xaxis.set_minor_locator(mdates.HourLocator(interval=1))  # Minor ticks every 1 hour
+plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=6))
 plt.legend()
 plt.xlabel('Time')
 plt.ylabel("Humidity")
+plt.xticks(rotation = 45)
 
-# Humidity BMP----------------------------------------------------------------------------------------------------------
-plt.subplot(3,2,4)
+#Atmosphere Pressure----------------------------------------------------------------------------------------------------
+plt.subplot(3,1,3)
+pressure = sensor[456]['values']
 
-humBMP = sensor[188]['values']
-y_hum_bmp = smooth(50, humBMP)
-plt.plot(x, y_hum_bmp, label = "Humidity BMP", color = 'red')
-plt.plot(x, non_linear(x,y_hum_bmp), label = "Model", color = 'blue')
-plt.axhline(float(np.mean(y_hum_bmp)), label="Mean", color='green')
-plt.axhline(float(np.std(y_hum_bmp)), label = "Standard Deviation", color = 'gold')
-plt.axhline(np.min(y_hum_bmp), label = "Minimum", color = 'purple')
-plt.axhline(np.max(y_hum_bmp), label = 'Maximum', color = 'pink')
-plt.axhline(float(np.median(y_hum_bmp)), label = "Median", color = 'orange')
 
-x_pred_time, y_pred_value = prediction_model(x_time,y_hum_bmp)
-plt.plot(x_pred_time, y_pred_value, label = "Prediction", color = "black")
-
-plt.title("Humidity DHT")
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
-plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=3))
-plt.gca().xaxis.set_minor_locator(mdates.HourLocator(interval=1))  # Minor ticks every 1 hour
-plt.legend()
-plt.xlabel('Time')
-plt.ylabel("Humidity ()")
-
-# pressure BMP----------------------------------------------------------------------------------------------------------
-plt.subplot(3,2,5)
-pressure = sensor[197]['values']
+hum = sensor[456]['values']
 y_atm = smooth(50, pressure)
-plt.plot(x, y_atm, label = "Humidity BMP", color = 'red')
-plt.plot(x, non_linear(x,y_atm), label = "Model", color = 'blue')
-plt.axhline(float(np.mean(y_atm)), label="Mean", color='green')
-plt.axhline(float(np.std(y_atm)), label = "Standard Deviation", color = 'gold')
-plt.axhline(np.min(y_atm), label = "Minimum", color = 'purple')
-plt.axhline(np.max(y_atm), label = 'Maximum', color = 'pink')
-plt.axhline(float(np.median(y_atm)), label = "Median", color = 'orange')
+x = list(np.arange(len(y_atm))) # list of number of index
+x_time = sensor[456]['timestamps'] #datetime format
 
+min_atm = np.min(y_hum)
+minX_atm = x_time[0]
+for i in range(len(sensor[456]['values'])):
+    if sensor[456]['values'][i] == minX_atm:
+        minX_atm = x[i]
+
+maxAtm = np.max(y_atm)
+maxX_bmp = x[0]
+for i in range(len(y_atm)):
+    if sensor[456]['values'][i] == maxAtm:
+        maxX_bmp = x_time[i]
+
+
+plt.plot(x,y_atm,label= "Temperature DHT",color='red')
+plt.plot(x, non_linear(x,y_atm),label = "Non-linear Model", color = 'blue')
+plt.axhline(float(np.mean(y_atm)), label="Mean", color='green')
+plt.axhline(float(np.std(y_atm)),label = "Standard Deviation", color = 'gold')
+plt.plot(np.max(y_atm), label = "Maximum", color = "pink")
+plt.axhline(float(np.median(y_atm)), label = "Median", color = "orange")
+print("Atmospheric Pressure Graph Succeed")
+#prediction
 x_pred_time, y_pred_value = prediction_model(x_time,y_atm)
 plt.plot(x_pred_time, y_pred_value, label = "Prediction", color = "black")
+print("Prediction Succeed")
 
 plt.title("Atmospheric Pressure")
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
-plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=3))
-plt.gca().xaxis.set_minor_locator(mdates.HourLocator(interval=1))  # Minor ticks every 1 hour
-
+plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=6))
 plt.legend()
 plt.xlabel('Time')
+plt.xticks(rotation = 45)
 plt.ylabel("Atmospheric Pressure (hPa)")
+plt.tight_layout()
 
+plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True, prune='both'))
 plt.show()
